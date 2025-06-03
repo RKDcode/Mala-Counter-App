1d82e0931d6dea7adeccd525f3497880e71c1b75
RKDcode <rishhav.dhiman@gmail.com>
2025-06-04 02:04:27 +0530

    Added additional counter
    single naam jap or mantra jap counter. To keep track of total jap done in single time.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let malaCountEl = document.getElementById("mala-count-el")
+
 let count = 0
+let malaCount = 0
+
+
 
 function increment() {
     count += 1
     countEl.textContent = count
+    if(count%108 === 0 && count>0){
+        malaCount += 1
+        malaCountEl.textContent = malaCount
+    }
 }
 
 function decrement() {
@@ -15,8 +24,10 @@ function decrement() {
 } 
 
 function save() {
-    let countStr = count + " - "
+    let countStr = malaCount + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
+    malaCountEl.textContent = 0
     count = 0
+    malaCount = 0
 }
